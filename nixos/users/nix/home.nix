3ac0af8c200ffe8329edd95e59d05a0c59d77e6c
chowe99 <chowej99@gmail.com>
2025-05-09 23:34:18 +0800

    fixed battery waybar

--- nixos/users/nix/home.nix
+++ nixos/users/nix/home.nix
@@ -90,7 +90,6 @@
           "keyboard-state"
           "sway/language"
           "battery"
-          "battery#bat2"
           "clock"
           "tray"
         ];
@@ -113,6 +112,18 @@
           };
           tooltip-format = " ";
           tooltip-format-disconnected = "";
+        };
+
+        battery = {
+          bat: "BAT2";
+          interval: 60;
+          states: {
+            warning: 30;
+            critical: 15;
+          },
+          format: "{capacity}% {icon}",;
+          format-icons: ["", "", "", "", ""];
+          max-length: 25;
         };
 
         tray = {
