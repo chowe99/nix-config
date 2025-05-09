99cc9af70c367cc498bb270be12d5ad28f1d6cff
chowe99 <chowej99@gmail.com>
2025-05-09 12:25:17 +0800

    WIP: zsh autocomplete added

--- nixos/users/nix/home.nix
+++ nixos/users/nix/home.nix
@@ -3,38 +3,30 @@
 
 {
   home.stateVersion = "24.11";
+
   home.packages = with pkgs; [
     vim git htop zsh
     zsh-autosuggestions
     zsh-syntax-highlighting
     zsh-fast-syntax-highlighting
-    neofetch btop
-    tree
+    neofetch btop tree
   ];
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+
     oh-my-zsh = {
       enable = true;
       theme = "simple";
+      plugins = [ "git" "sudo" "z" ];
     };
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.zsh-autosuggestions;
-        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.zsh-syntax-highlighting;
-        file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
-      }
-      {
-        name = "fast-syntax-highlighting";
-        src = pkgs.zsh-fast-syntax-highlighting;
-        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-      }
-    ];
+
+    # Ensure completion is loaded correctly
+    completionInit = ''
+      autoload -U compinit
+      compinit
+    '';
   };
 
   programs.git = {
@@ -43,13 +35,13 @@
     userEmail = "chowej99@gmail.com";
   };
 
-
   home.file.".zshrc".text = ''
     export EDITOR=vim
     export PATH=$HOME/bin:$PATH
     alias vim=lvim
-    eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json')"
 
+    # Oh My Posh init
+    eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json')"
   '';
 }
 
