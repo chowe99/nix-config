5bcfb35905b9cfe14bfb9f2d2b23b7cb34beac17
chowe99 <chowej99@gmail.com>
2025-05-09 21:28:05 +0800

    updated waybar config

--- nixos/users/nix/waybar/start_bars.sh
+++ nixos/users/nix/waybar/start_bars.sh
@@ -1,2 +1,4 @@
-#!/bin/zsh
-waybar --config $HOME/.config/waybar/dp1.jsonc
+# Launch Waybar for the primary display (eDP-1)
+waybar -c ~/.config/waybar/eDP-1.jsonc -s ~/.config/waybar/style.css 
+
+
