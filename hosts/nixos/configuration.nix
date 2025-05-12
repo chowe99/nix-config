3219f6349f4bbeb0c4c7fd5d1cb68aede70ac96a
chowe99 <chowej99@gmail.com>
2025-05-13 05:02:29 +0800

    hyprland fix?

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -64,7 +64,10 @@
   nixpkgs.config.allowUnfree = true;
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
   services.tailscale.enable = true;
   environment.systemPackages = with pkgs; [
