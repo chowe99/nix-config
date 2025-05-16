0cc7d52372e6819a34532d609ee85e79d50096a1
chowe99 <chowej99@gmail.com>
2025-05-16 18:02:49 +0800

    removing wayland config temporarily

--- nixos/users/nix/home.nix
+++ nixos/users/nix/home.nix
@@ -103,18 +103,18 @@ in
       };
     };
   programs.kitty.enable = true;
-  wayland.windowManager.hyprland = {
-    enable = true;
-    # Use Hyprland and xdg-desktop-portal-hyprland from NixOS module
-    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    package = pkgs.hyprland;
-    # portalPackage = null;
-    # Load custom configuration
-    # This assumes hyprland.conf is in the same directory as home.nix
-    # extraConfig = builtins.readFile ./hyprland.conf;
-    # Optional: If you have issues with systemd services not finding programs
-    # systemd.variables = ["--all"];
-  };
+  # wayland.windowManager.hyprland = {
+  #   enable = true;
+  #   # Use Hyprland and xdg-desktop-portal-hyprland from NixOS module
+  #   # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  #   package = pkgs.hyprland;
+  #   # portalPackage = null;
+  #   # Load custom configuration
+  #   # This assumes hyprland.conf is in the same directory as home.nix
+  #   # extraConfig = builtins.readFile ./hyprland.conf;
+  #   # Optional: If you have issues with systemd services not finding programs
+  #   # systemd.variables = ["--all"];
+  # };
 
   programs.rofi = {
     enable = true;
