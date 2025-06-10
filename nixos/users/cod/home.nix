81c89193a99b96d52dc4e8189d391c9c0f270f29
chowe99 <chowej99@gmail.com>
2025-06-10 22:07:37 +0800

    asahi 1.1

--- nixos/users/cod/home.nix
+++ nixos/users/cod/home.nix
@@ -62,6 +62,10 @@
       };
     };
 
+  programs.kitty.enable = true;
+  wayland.windowManager.hyprland.enable = true;
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
+
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
