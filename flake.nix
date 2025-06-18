70b43488a882fe2cb59a852f2929f20b8aaff6cf
chowe99 <chowej99@gmail.com>
2025-06-18 14:00:35 +0800

    working on templates

--- flake.nix
+++ flake.nix
@@ -1,127 +1,99 @@
-{
-  description = "Flake-based NixOS + Home Manager config";
+{ description = "Flake-based NixOS + Home Manager config";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Added for unstable packages
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dotfiles = {
-      url = "github:chowe99/dotfiles";
-      flake = false;
-    };
-
-    nix-flatpak = {
-      url = "github:gmodena/nix-flatpak";
-    };
+  home-manager = {
+    url = "github:nix-community/home-manager/release-24.11";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, agenix, nix-flatpak, ... }@inputs: {
-    nixosConfigurations = {
-      nix = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          hostname = "nix";
-          username = "nix";
-        };
-        modules = [
-          ./hosts/nixos/configuration.nix
-          inputs.agenix.nixosModules.default
-          inputs.home-manager.nixosModules.home-manager
-          {
-            environment.systemPackages = with nixpkgs.legacyPackages."x86_64-linux"; [
-              inputs.agenix.packages."x86_64-linux".default
-            ];
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.nix = import ./nixos/users/nix/home.nix;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              hostname = "nix";
-              username = "nix";
-            };
-          }
-        ];
-      };
+  agenix = {
+    url = "github:ryantm/agenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-      whiteserver = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          hostname = "whiteserver";
-          username = "whiteserver";
-        };
-        modules = [
-          ./hosts/whiteserver/configuration.nix
-          inputs.agenix.nixosModules.default
-          inputs.home-manager.nixosModules.home-manager
-          {
-            environment.systemPackages = with nixpkgs.legacyPackages."x86_64-linux"; [
-              inputs.agenix.packages."x86_64-linux".default
-            ];
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.whiteserver = import ./nixos/users/whiteserver/home.nix;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              hostname = "whiteserver";
-              username = "whiteserver";
-            };
-          }
-        ];
-      };
+  nix-flatpak = {
+    url = "github:gmodena/nix-flatpak";
+  };
 
-      blackserver = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          hostname = "blackserver";
-          username = "blackserver";
-        };
-        modules = [
-          ./hosts/blackserver/configuration.nix
-          inputs.agenix.nixosModules.default
-          inputs.home-manager.nixosModules.home-manager
-          {
-            environment.systemPackages = with nixpkgs.legacyPackages."x86_64-linux"; [
-              inputs.agenix.packages."x86_64-linux".default
-            ];
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.blackserver = import ./nixos/users/blackserver/home.nix;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              hostname = "blackserver";
-              username = "blackserver";
-            };
-          }
-        ];
-      };
+  nixvim = {
+    url = "github:nix-community/nixvim";
+    inputs.nixpkgs.follows = "nixpkgs-unstable";
+  };
+
+  dotfiles = {
+    url = "github:chowe99/dotfiles";
+    flake = false;
+  };
+};
+
+outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, agenix, nix-flatpak, nixvim, ... }@inputs: let
+  systems = [ "x86_64-linux" "aarch64-linux" ];
+  mkNixOS = host: nixpkgs.lib.nixosSystem {
+    system = host.system;
+    specialArgs = { inherit inputs; hostname = host.name; username = host.user; };
+    modules = [
+      ./hosts/${host.name}/configuration.nix
+      inputs.agenix.nixosModules.default
+      inputs.home-manager.nixosModules.home-manager
+      # Minimal system-specific only
+    ];
+  };
+  hosts = [
+    { name = "nix"; system = "x86_64-linux"; user = "nix"; }
+    { name = "whiteserver"; system = "x86_64-linux"; user = "whiteserver"; }
+    { name = "blackserver"; system = "x86_64-linux"; user = "blackserver"; }
+  ];
+in {
+  nixosConfigurations = builtins.listToAttrs (builtins.map (host: {
+    name = host.name;
+    value = mkNixOS host;
+  }) hosts);
+
+  homeConfigurations = {
+    # cod user on aarch64
+    cod = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.aarch64-linux;
+      extraSpecialArgs = { inherit inputs; hostname = "cod"; username = "cod"; };
+      modules = [
+        nix-flatpak.homeManagerModules.nix-flatpak
+        nixvim.homeManagerModules.nixvim
+        ./nixos/users/cod/home.nix
+      ];
     };
 
-    homeConfigurations = {
-      cod = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."aarch64-linux";
-        extraSpecialArgs = {
-          inherit inputs;
-          hostname = "cod";
-          username = "cod";
-        };
-        modules = [
-          nix-flatpak.homeManagerModules.nix-flatpak
-          ./nixos/users/cod/home.nix
-        ];
-      };
+    # other x86 users
+    nix = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; hostname = "nix"; username = "nix"; };
+      modules = [
+        nix-flatpak.homeManagerModules.nix-flatpak
+        nixvim.homeManagerModules.nixvim
+        ./nixos/users/nix/home.nix
+      ];
+    };
+
+    whiteserver = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; hostname = "whiteserver"; username = "whiteserver"; };
+      modules = [
+        nix-flatpak.homeManagerModules.nix-flatpak
+        nixvim.homeManagerModules.nixvim
+        ./nixos/users/whiteserver/home.nix
+      ];
+    };
+
+    blackserver = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; hostname = "blackserver"; username = "blackserver"; };
+      modules = [
+        nix-flatpak.homeManagerModules.nix-flatpak
+        nixvim.homeManagerModules.nixvim
+        ./nixos/users/blackserver/home.nix
+      ];
     };
   };
 }
+
