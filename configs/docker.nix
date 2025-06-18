ec4c2281f7b28f3d1809e1b62983fb0c0a0c4733
chowe99 <chowej99@gmail.com>
2025-06-18 22:15:10 +0800

    moved caddy to port 3000

--- configs/docker.nix
+++ configs/docker.nix
@@ -21,7 +21,7 @@
   virtualisation.oci-containers.containers = {
     homepage = {
       image = "ghcr.io/gethomepage/homepage:latest";
-      ports = [ "3000:3030" ];
+      ports = [ "3000:3000" ];
       environment = {
         PUID = "1000";
         PGID = "1000";
