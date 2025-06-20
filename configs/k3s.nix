f0e7b2b7491ea45344b220e909c8f4acb73618eb
chowe99 <chowej99@gmail.com>
2025-06-20 11:29:49 +0800

    temp disable k3s

--- configs/k3s.nix
+++ configs/k3s.nix
@@ -11,11 +11,11 @@
   #   openFirewall = true;
   # };
 
-  services.k3s = {
-    enable = true;
-    role = "server";
-    extraFlags = "--datastore-endpoint=etcd://whiteserver:2379,blackserver:2379,asusserver:2379 --cluster-init";
-  };
+  # services.k3s = {
+  #   enable = true;
+  #   role = "server";
+  #   extraFlags = "--datastore-endpoint=etcd://whiteserver:2379,blackserver:2379,asusserver:2379 --cluster-init";
+  # };
 
   networking.firewall.allowedTCPPorts = [ 80 443 6443 2379 2380 24007 24008 49152 49153 ];
   networking.firewall.allowedUDPPorts = [ 8472 ];
