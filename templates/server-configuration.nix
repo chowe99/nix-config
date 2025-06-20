be2f2d49f7b06bc18fe74791c49889efd0a81b74
chowe99 <chowej99@gmail.com>
2025-06-20 12:00:08 +0800

    temp firewall disable

--- templates/server-configuration.nix
+++ templates/server-configuration.nix
@@ -143,9 +143,9 @@ in
   ];
 
   services.openssh.enable = true;
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 22 80 443 3000 3030];
+  # networking.firewall = {
+  #   enable = true;
+  #   allowedTCPPorts = [ 22 80 443 3000 3030];
   };
 
   security.sudo = {
@@ -155,13 +155,6 @@ in
     '';
   };
   
-  # Enable k3s (Server 1 as control plane)
-  #services.k3s = {
-    #enable = true;
-    #role = "server";
-    #token = k3sToken;
-    #extraFlags = "--disable traefik"; # Disable default Traefik for custom ingress
-  #};
 
   # Enable GlusterFS
   # services.glusterfs = {
