878594545728707eb953c55428d4aaf554d5fc79
chowe99 <chowej99@gmail.com>
2025-06-22 02:43:52 +0800

    removed whiteserver k3s ip

--- hosts/blackserver/configuration.nix
+++ hosts/blackserver/configuration.nix
@@ -9,7 +9,6 @@
   ];
 
   services.k3s.role = "server";
-  services.k3s.serverAddr = "https://100.64.65.24:6443";
   services.k3s.tokenFile = "/run/agenix/k3s-token";
   services.k3s.extraFlags = "--disable=traefik --cluster-init";
 }
