2669a9cda5ee55ee68ebc6fcbb7704f75dd53359
chowe99 <chowej99@gmail.com>
2025-07-02 12:10:09 +0800

    removed cluster--init

--- hosts/blackserver/configuration.nix
+++ hosts/blackserver/configuration.nix
@@ -11,7 +11,6 @@
        tokenFile = "/run/agenix/k3s-token";
        extraFlags = toString [
          "--disable=traefik"
-         "--cluster-init"
          "--server https://10.1.1.249:6443"
          "--advertise-address=10.1.1.250"
          "--node-ip=10.1.1.250"
