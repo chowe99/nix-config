209368c0caa358dd0dde46fb0e2eed3a22b455e6
chowe99 <chowej99@gmail.com>
2025-07-03 16:31:23 +0800

    disabled k3s for now

--- hosts/whiteserver/configuration.nix
+++ hosts/whiteserver/configuration.nix
@@ -9,21 +9,21 @@
       # (import ../../configs/nixvim.nix { inherit inputs; })
   ];
 
-  services.k3s = {
-    role = "server";
-    tokenFile = "/run/agenix/k3s-token";
-    extraFlags = toString [
-      "--disable=traefik"
-        "--cluster-init"
-        "--advertise-address=10.1.1.249"
-        "--node-ip=10.1.1.249"
-        "--node-name=whiteserver"
-        # "--tls-san=10.1.1.249"
-        # "--tls-san=127.0.0.1"
-        # "--tls-san=k3s.howse.top"
-        # "--tls-san=dashboad.howse.top"
-    ];
-  };
+  # services.k3s = {
+  #   role = "server";
+  #   tokenFile = "/run/agenix/k3s-token";
+  #   extraFlags = toString [
+  #     "--disable=traefik"
+  #       "--cluster-init"
+  #       "--advertise-address=10.1.1.249"
+  #       "--node-ip=10.1.1.249"
+  #       "--node-name=whiteserver"
+  #       # "--tls-san=10.1.1.249"
+  #       # "--tls-san=127.0.0.1"
+  #       # "--tls-san=k3s.howse.top"
+  #       # "--tls-san=dashboad.howse.top"
+  #   ];
+  # };
 
 
   fileSystems."/mnt/nas" = {
