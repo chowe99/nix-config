6bb4fc643d35adc0829ec8da2a8c13e894fc1493
chowe99 <chowej99@gmail.com>
2025-07-04 20:30:12 +0800

    enabled docker blackserverg

--- hosts/blackserver/configuration.nix
+++ hosts/blackserver/configuration.nix
@@ -1,24 +1,25 @@
 { config, pkgs, inputs, username, ... }:
-   {
-     imports = [
-       ./hardware-configuration.nix
-       ../../templates/server-configuration.nix
-       ../../configs/k3s.nix
-     ];
+{
+  imports = [
+    ./hardware-configuration.nix
+      ../../templates/server-configuration.nix
+      ../../configs/k3s.nix
+      ../../configs/docker.nix
+  ];
 
-     services.k3s = {
-       role = "server";
-       tokenFile = "/run/agenix/k3s-token";
-       extraFlags = toString [
-         # "--disable=traefik"
-         "--server https://10.1.1.249:6443"
-         # "--advertise-address=10.1.1.250"
-         "--node-ip=10.1.1.250"
-         "--node-name=blackserver"
-         # "--tls-san=10.1.1.250"
-         # "--tls-san=127.0.0.1"
-       ];
-     };
+  services.k3s = {
+    role = "server";
+    tokenFile = "/run/agenix/k3s-token";
+    extraFlags = toString [
+# "--disable=traefik"
+      "--server https://10.1.1.249:6443"
+# "--advertise-address=10.1.1.250"
+        "--node-ip=10.1.1.250"
+        "--node-name=blackserver"
+# "--tls-san=10.1.1.250"
+# "--tls-san=127.0.0.1"
+    ];
+  };
 
   virtualisation.oci-containers.containers = {
     gitlab = {
@@ -26,12 +27,12 @@
       hostname = "git.howse.top";
       ports = [
         "1480:80"
-        "2222:22"
+          "2222:22"
       ];
       volumes = [
         "/mnt/nas/gitlab/config:/etc/gitlab"
-        "/mnt/nas/gitlab/logs:/var/log/gitlab"
-        "/mnt/nas/gitlab/data:/var/opt/gitlab"
+          "/mnt/nas/gitlab/logs:/var/log/gitlab"
+          "/mnt/nas/gitlab/data:/var/opt/gitlab"
       ];
       extraOptions = [
         "--network=server_network"
@@ -40,34 +41,34 @@
     };
   };
 
-    # networking.firewall = {
-    #   enable = true;
-    #   allowedTCPPorts = [ 52631 6443 2379 2380 10250 24007 24008 49152 49153 49154 ];
-    #   allowedUDPPorts = [ 8472 24007 24008 ];
-    # };
+# networking.firewall = {
+#   enable = true;
+#   allowedTCPPorts = [ 52631 6443 2379 2380 10250 24007 24008 49152 49153 49154 ];
+#   allowedUDPPorts = [ 8472 24007 24008 ];
+# };
 
-     networking.hosts = {
-       "10.1.1.249" = [ "whiteserver" ];
-       "10.1.1.250" = [ "blackserver" ];
-       "10.1.1.64" = [ "asusserver" ];
-     };
+  networking.hosts = {
+    "10.1.1.249" = [ "whiteserver" ];
+    "10.1.1.250" = [ "blackserver" ];
+    "10.1.1.64" = [ "asusserver" ];
+  };
 
-     boot.swraid.enable = true;
-     boot.swraid.mdadmConf = ''
-       ARRAY /dev/md127 UUID=e22f6488:83684aca:f30ec314:f49242d1
-       MAILADDR c0dred@tutamail.com
-       '';
+  boot.swraid.enable = true;
+  boot.swraid.mdadmConf = ''
+    ARRAY /dev/md127 UUID=e22f6488:83684aca:f30ec314:f49242d1
+    MAILADDR c0dred@tutamail.com
+    '';
 
-    # Filesystem configuration for RAID
-    fileSystems."/mnt/nas" = {
-      device = "/dev/md127";
-      fsType = "ext4";
-      options = [ "defaults" "nofail" ];
-    };
+# Filesystem configuration for RAID
+  fileSystems."/mnt/nas" = {
+    device = "/dev/md127";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
 
-     systemd.tmpfiles.rules = [
-       "d /mnt/nas/glusterfs/nextcloud 0755 ${username} root -"  # For whiteserver, blackserver
-     ];
+  systemd.tmpfiles.rules = [
+    "d /mnt/nas/glusterfs/nextcloud 0755 ${username} root -"  # For whiteserver, blackserver
+  ];
 
-     age.secrets.k3s-token.file = ../../secrets/k3s-token.age;
-   }
+  age.secrets.k3s-token.file = ../../secrets/k3s-token.age;
+}
