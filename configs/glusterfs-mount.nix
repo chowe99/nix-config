debe9cb3403dabdfd81d8261218a64ffef67ec99
chowe99 <chowej99@gmail.com>
2025-07-05 12:55:37 +0800

    changed to systed.mounts

--- configs/glusterfs-mount.nix
+++ configs/glusterfs-mount.nix
@@ -18,18 +18,27 @@
     "d /var/lib/nextcloud/data 0750 nextcloud nextcloud - -"
   ];
 
-  # Configure the mount in fstab
-  fileSystems."/var/lib/nextcloud/data" = {
-    device = "10.1.1.249:/nextcloud-vol";  # Use whiteserver IP
-    fsType = "glusterfs";
-    options = [
-      "defaults"
-      "acl"
-      "_netdev"
-      # "backupvolfile-server=10.1.1.250"  # Use blackserver as backup
-      "log-level=WARNING"
-      "log-file=/var/log/gluster.log"
-    ];
-  };
+  # # Configure the mount in fstab
+  # fileSystems."/var/lib/nextcloud/data" = {
+  #   device = "10.1.1.249:/nextcloud-vol";  # Use whiteserver IP
+  #   fsType = "glusterfs";
+  #   options = [
+  #     "defaults"
+  #     "acl"
+  #     "_netdev"
+  #     # "backupvolfile-server=10.1.1.250"  # Use blackserver as backup
+  #     "log-level=WARNING"
+  #     "log-file=/var/log/gluster.log"
+  #   ];
+  # };
+
+  systemd.mounts = [
+    {
+      what = "10.1.1.249:/nextcloud-vol";
+      where = "/var/lib/nextcloud/data";
+      type = "glusterfs";
+      options = "defaults,acl,_netdev,log-level=WARNING,log-file=/var/log/gluster.log";
+    }
+  ];
 }
 
