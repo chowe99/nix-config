9b0550df20d5e837f64678c08ea372a2163f981b
chowe99 <chowej99@gmail.com>
2025-07-05 12:07:52 +0800

    moving caddy to asusserver and added nextloud ports to firewall

--- hosts/whiteserver/configuration.nix
+++ hosts/whiteserver/configuration.nix
@@ -3,7 +3,6 @@
   imports = [
     ./hardware-configuration.nix
       ../../templates/server-configuration.nix
-      ../../configs/caddy.nix
       ../../configs/docker.nix
       ../../configs/k3s.nix
       ../../configs/glusterfs-mount.nix
