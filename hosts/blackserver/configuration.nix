6f965958d737e433d146336cb185dacd165803b3
chowe99 <chowej99@gmail.com>
2025-07-05 23:41:40 +0800

    moved keebs website to whiteserver

--- hosts/blackserver/configuration.nix
+++ hosts/blackserver/configuration.nix
@@ -21,15 +21,15 @@
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.bash}/bin/bash /home/blackserver/keebs/start-keebs.sh";
-      WorkingDirectory = "/home/blackserver/keebs/";
+      ExecStart = "${pkgs.bash}/bin/bash /home/${username}/keebs/start-keebs.sh";
+      WorkingDirectory = "/home/${username}/keebs/";
       Restart = "always";
       RestartSec = "5s";
       Environment = [
         "PORT=3008"
         "NODE_ENV=development"
       ];
-      User = "blackserver";
+      User = "${username}";
       Group = "users";
     };
   };
