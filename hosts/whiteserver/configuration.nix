077dcc502fc70440ea9bdc82b5ccdff6277cae6b
chowe99 <chowej99@gmail.com>
2025-07-10 09:19:34 +0800

    moved caddy to asusserver

--- hosts/whiteserver/configuration.nix
+++ hosts/whiteserver/configuration.nix
@@ -3,7 +3,7 @@
   imports = [
     ./hardware-configuration.nix
       ../../templates/server-configuration.nix
-      ../../configs/caddy.nix
+      # ../../configs/caddy.nix
       ../../configs/docker.nix
       ../../configs/k3s.nix
       ../../configs/gitlab.nix
